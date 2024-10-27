0e944c594a6c4f2c3c191763a1ed664dcf5e3967
Soham Bose <112571571+shb2908@users.noreply.github.com>
2024-10-27 11:32:18 +0530

    Add files via upload

---
+++ CN-4/const.py
@@ -0,0 +1,10 @@
+import random
+
+total_sender_number       = 4
+total_receiver_number     = 4
+default_data_packet_size  = 1
+
+sender_sleep_time         = random.uniform(0.3, 0.8)
+
+input_file_path           = "./textfiles/input/"
+output_file_path          = "./textfiles/output/"
